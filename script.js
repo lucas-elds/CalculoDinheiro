644d191e4902fb03990a7f97e1db38b4b8d3ce37
Lucas <lucasemiliano619@gmail.com>
2024-09-02 20:57:18 -0300

    Adição do cálculo de centavos

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let recebido = document.querySelector("#recebido");
 let valor = document.querySelector("#valor");
 const troco = document.querySelector("#troco");
 let notasHTML = document.querySelector("#notas");
-const notas = [200, 100, 50, 20, 10, 5, 2, 1];
+const notas = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01];
 const cedulas = [
   "img/real200.png",
   "img/real100.png",
@@ -12,11 +12,16 @@ const cedulas = [
   "img/real5.png",
   "img/real2.png",
   "img/real1.png",
+  "img/centavo50.png",
+  "img/centavo25.png",
+  "img/centavo10.png",
+  "img/centavo5.png",
+  "img/centavo1.png",
 ];
 
 function calcular() {
   let calculo = recebido.value - valor.value;
-  notasHTML.innerHTML = `<h2>Troco: ${calculo}</h2>`;
+  notasHTML.innerHTML = `<h2>Troco: R$ ${calculo.toFixed(2)}</h2>`;
   while (calculo > 0) {
     for (let i = 0; i < notas.length; i++) {
       if (notas[i] <= calculo) {
@@ -27,6 +32,8 @@ function calcular() {
         notasHTML.appendChild(item);
         notasHTML.appendChild(imagem);
         calculo = calculo - notas[i];
+        calculo = calculo.toFixed(2);
+        calculo = parseFloat(calculo);
         break;
       }
     }
